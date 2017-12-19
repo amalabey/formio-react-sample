11b8bd29b1cf5b86e7f802be1d78a47466792f6e
Amal Abeygunawardana <amal.abeygunawardana@team.telstra.com>
2017-12-19 21:38:12 +1100

    chrome debugging works

--- src/components/LeaveformComponent.jsx
+++ src/components/LeaveformComponent.jsx
@@ -1,6 +1,7 @@
 'use strict';
 
 import React from 'react';
+import {Formio} from 'react-formio';
 
 require('styles//Leaveform.css');
 
@@ -20,6 +21,15 @@ class LeaveformComponent extends React.Component {
     console.log("--> In Save event");
     this.setState({leaveBalance: this.state.leaveBalance + 1});
   }
+  onLeaveSubmit(submission){
+    console.log("---> leave submitted");
+  }
+  onLeaveFormError(response){
+    console.log("--> error occurred : "+response);
+  }
+  onLeaveFormPropChange(submission, key, value){
+    console.log("--> "+key+" changed-to "+value);
+  }
   render() {
     console.log("--> In render event");
     return (
@@ -27,8 +37,13 @@ class LeaveformComponent extends React.Component {
         <h1> Hi There - ! {this.props.title}</h1>
         <p>Your leave balance: {this.state.leaveBalance}</p>
         <div>
-          <input type="button" onClick={this.onSave.bind(this)} value="Save"></input>
+          <input type="button" onClick={this.onSave.bind(this)} value="Check Leave"></input>
         </div>
+        <hr/>
+        <Formio src="https://uhg.medebridge2.com/testhidden" 
+                onFormSubmit={this.onLeaveSubmit.bind(this)}
+                onFormError={this.onLeaveFormError.bind(this)}
+                onChange={this.onLeaveFormPropChange.bind(this)}/>
       </div>
     );
   }
